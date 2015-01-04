a83501996beb23de3f4f875733f2c7de1b6a7770
Reuven Roth <reuvenroth@users.noreply.github.com>
2015-01-04 18:46:49 -0500

    Added delete link on @article show page. GET request instead of DELETE for HTML so no delete yet.

--- rails_project/blogger/app/controllers/articles_controller.rb
+++ rails_project/blogger/app/controllers/articles_controller.rb
@@ -1,18 +1,22 @@
 class ArticlesController < ApplicationController
 	include ArticlesHelper
+	# set homepage to list all articles
 	def index
 		@articles = Article.all
 	end
+	# retrieve & display all articles
 	def show
 		@article = Article.find(params[:id])
 	end
+	# add a new article from input
 	def new
 		@article = Article.new
 	end
+	# add a new article into the database
 	def create
 		@article = Article.new(article_params)
 		@article.save
-		
+
 		redirect_to article_path(@article)
 	end
 end
