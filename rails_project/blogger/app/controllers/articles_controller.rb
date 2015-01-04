b5275c78781d92cf471a2c1ddef430921d7f3657
Reuven Roth <reuvenroth@users.noreply.github.com>
2015-01-04 00:41:04 -0500

    Stores Title & Body data cleaned with weak params. Update to Strong Params with require & permit

--- rails_project/blogger/app/controllers/articles_controller.rb
+++ rails_project/blogger/app/controllers/articles_controller.rb
@@ -9,10 +9,8 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def create
-  		@article = Article.new(
-    	title: params[:article][:title],
-    	body: params[:article][:body])
-  		@article.save
-  		redirect_to article_path(@article)
+		@article = Article.new(params[:article])
+		@article.save
+		redirect_to article_path(@article)
 	end
 end
