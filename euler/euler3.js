df56e5f613c6fdcbf783915822b3ec18c7e10043
Reuven Roth <reuvenroth@users.noreply.github.com>
2015-01-12 23:37:01 -0500

    Euler 3 first attempt

---
+++ euler/euler3.js
@@ -0,0 +1,25 @@
+sum = 0
+function prime(num) {
+	num ? % 2 !== 0 && num ? % 3 !== 0 && num ? % 5 !== 0 && num ? % 7 !== 0 {
+		return sum += num
+	}
+}
+num = 13195;
+for (i = 0; i <= num; i++) {
+	if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
+		console.log(num);
+	}
+}
+(function () {
+  var primeFactors = [];
+  var number = 600851475143;
+  var i = 2;
+  while (number > 1) {
+    while (number % i === 0) {
+      primeFactors.push(i);
+      number /= i;
+    }
+    i++;
+  }
+  alert(primeFactors[primeFactors.length - 1]);
+})();
