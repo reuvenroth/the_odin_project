b42795342ded4e786f958bf484cd8b2d0b24c213
Reuven Roth <reuvenroth@users.noreply.github.com>
2015-01-14 22:55:53 -0500

    Euler 3

--- euler/euler3.js
+++ euler/euler3.js
@@ -5,7 +5,7 @@ function prime(num) {
 		return sum += num
 	}
 }
-
+//individual test spelled out
 num = 13195;
 for (i = 0; i <= num; i++) {
 	if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
